ebe298a64b94503962bbbc1a723d7df99b7f72db
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-09 05:47:35 -0800

    Prog 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**   
+* main -"Programming is like building a multilingual puzzle"
+* Return: Always 0
+*/  
+int main(void)   
+{
+	printf("/"with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+} 
