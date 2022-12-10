eb316c33ba3e49b55fe7e2fe70a29f1d25142c9c
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-10 11:37:49 -0800

    Prog 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-/*  
-* main -"Programming is like building a multilingual puzzle"
+/**
+* main - print a
+*
+* Description: A
 * Return: Always (0);
-*/  
-main(void)   
+*/
+
+
+int main(void)   
+
 {
-	printf("/"with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-} 
+    puts("\"Programming is like building a multilingual puzzle);
+     return (0);
+}
