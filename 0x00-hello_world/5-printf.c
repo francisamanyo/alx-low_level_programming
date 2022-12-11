a05ac2b48256680c641785bbe6a510931c69ebbf
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-11 08:59:24 -0800

    Prog 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
-
+#include <stdio.h
 /**
  *
- * * main - print a c program using printf
+ * main - print a c program using printf
+ * Return: 0
  *
- * *
- *
- * * Return: 0
- *
- * */
-
-
+ */
 
 int main(void)
-
 {
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
