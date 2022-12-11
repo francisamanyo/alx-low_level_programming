6504d0887eaf5b5e8f2aea17d325d96280193a4f
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-11 00:09:54 -0800

    Var 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (success)
+ *
+ * */
+int main(void)
+{
+	        int a;
+		        for (a = 0; a < 10; a++)
+				                printf("%d", a);
+
+			        putchar('\n');
+				        return (0);
+}
+
+
+
+6. Numberz
+
+#include <stdio.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (success)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	        int c;
+
+
+
+		        c = 0;
+
+			        while
+
+					                (c < 10) {
+
+								                        putchar(c + '0');
+
+											                        c++;
+
+														                }
+
+				        putchar('\n');
+
+					        return (0);
+
+}
