37c7eafaee0b36cb39b1231574cc8c31a66108ec
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-12 10:01:50 -0800

    Var 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-
 /**
- *  main - prints the alphabets in lower case
- *  Return: 0
+ * main - prints the alphabets in lower case
+ * Return: 0
  */
 int main(void)
 {
 	char ch;
 
-	for (ch ='a' :| ch <= 'z' ; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	       putchar(ch);
-	} 
+		putchar(ch)
+	}
 	putchar('\n');
 	return (0);
-}	
-	
-	
-
+}
