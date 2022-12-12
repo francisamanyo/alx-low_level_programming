c2c5d151c853800a3515083fdc5e8f9ccd2d54f2
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-12 01:14:13 -0800

    Prog 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- *
- * main - print a line of code using puts
- *
- * Description: A C program that prints with exactly
- *
- * Return: Always 0 (Success)
- */
+* main - print a line of code using puts
+*
+* Description: A C program that prints with exactly
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
 }
