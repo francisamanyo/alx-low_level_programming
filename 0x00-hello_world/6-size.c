64851da31369143a2e031d60d1e0e1c1708475aa
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-12 08:10:49 -0800

    Prog 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-/** 
+#include<stdio.h>
+
+/**
 * main - print out various sizes of data types
 * code by Francis Amanyo
 * Return: 0
-*\
+*/
+
 int main(void)
 {
 	char a;
 	int b;
-	long int c;long long int d;
+	long int c;
+	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n" ,(unassigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n" ,(unassigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n" ,(unassigned long)sizeof(c));	print("Size of a long long int: %lu byte(s)\n" ,(unassigned long)size of(d));
-	printf("Size of a float: %lu byte(s)\n" ,(unassigned long)size of(f));
-	return (0)
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
-
