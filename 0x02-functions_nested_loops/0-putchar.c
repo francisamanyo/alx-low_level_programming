15cb5327f74592214a8c4eebe526aa0f211520e9
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-13 13:27:25 -0800

    Loop 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
 
