1ca327867e0aa197e9550a958299aeacf8667c7e
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-13 14:44:20 -0800

    Loop 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print _alphabet - prints the alpha in lowercase
  */
-* Description : print all function prototype used in this project
+* Description: print all function prototype used in this project
 void print_alphabet(void)
 {
 	char la;
