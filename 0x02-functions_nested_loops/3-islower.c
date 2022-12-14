eacea19f1a0f4c6fa133d273725b413a9615b854
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-14 08:21:47 -0800

    Loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 # include "main.h"
 /**
- * _is lower - check if the character is lowercase
+ * _islower - check if the character is lowercase
  * @c: the character
  * Return: 1 if letter is lowercase, 0 if not
  */
-int_islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
