794a9fdb0d6324d4f9a12c62bcc4492e392609dd
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-14 03:47:21 -0800

    Loop 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print _alphabet - prints the alpha in lowercase
- */
-* Description: print all function prototype used in this project
+* print _alphabet - prints the alpha in lowercase
+*/
 void print_alphabet(void)
 {
-	char la;
+	char letter;
 
-	for (la = 'a'; la <= 'z'; la++)
-		_putchar(la);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 	_putchar('\n');
 }
