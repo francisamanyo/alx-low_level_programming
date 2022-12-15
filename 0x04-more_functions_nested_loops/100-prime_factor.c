b1928da11a31c99a69a693c3a964f8df0942d5a9
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-15 13:33:08 -0800

    More 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*main - prints the largest prime factor of 612852475143
-*francis / Amanyo
+* main - prints the largest prime factor of 612852475143
+* francis / Amanyo
 * Return: always 0
 *
 **/
@@ -10,11 +10,12 @@
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
+
 	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
 	}
-	printf(" % lu\n", n);
+	printf("%lu\n", n);
 	return (0);
 }
