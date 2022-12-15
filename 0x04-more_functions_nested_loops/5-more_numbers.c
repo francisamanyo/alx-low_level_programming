85257a6f87357a4c4807791f4032245238ff19bb
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-15 11:28:19 -0800

    More 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,6 +10,7 @@ void more_numbers(void)
 {
 
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
@@ -17,7 +18,7 @@ void more_numbers(void)
 			if (j >= 10)
 				_putchar('1');
 			_putchar(j % 10 + '0');
-			{
+		}
 				_putchar('\n');
 			}
 }
