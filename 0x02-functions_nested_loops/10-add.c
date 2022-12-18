cba21fc5683a2a3893931c88f11adf7903c3f27c
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-18 05:00:55 -0800

    Loop 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
 * @num1: The first integer to be added
 * @num2: The second integer to be added
 *
-* Return : The result of the addition
+* Return: The result of the addition
 */
 int add(int num1, int num2)
 {
