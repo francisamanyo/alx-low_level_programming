efb5d4660393baa23f53d3d344c239edd0e601ea
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2022-12-18 04:30:47 -0800

    Loop 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
-*print _alphabet_x10-Prints 10 times, in lower case
+* print _alphabet_x10-Prints 10 times, in lower case
 */
 void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
+
 	while (count++ <= 9)
-}
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
