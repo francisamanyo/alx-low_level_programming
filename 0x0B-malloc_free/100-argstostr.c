a949b1b64ee3ab6c199a54195a630f1a7d01e043
francisamanyo <amanyofrancigit config --global user.email amanyofrancis@yahoo.com>
2023-01-10 14:14:24 -0800

    Mar 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- **argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- *
- * Return: 0
- */
+* *argstostr - main entry
+* @ac: int input
+* @av: double pointer array
+*
+* Return: 0
+*/
 char *argstostr(int ac, char **av)
 {
 	 int i, n, r = 0, l = 0;
